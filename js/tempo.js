37f23a91602aa641f7326fa92dc40c8c33143916
João Guedes <55167438+joao-tads@users.noreply.github.com>
2020-01-24 03:14:23 -0300

    Create tempo.js

---
+++ js/tempo.js
@@ -0,0 +1,61 @@
+var getUrl = "https://tads-kitchen.herokuapp.com/cards";
+var atualizar = setInterval(temporizador, 5000);
+
+$(document).ready(function() {
+    $.get(
+        getUrl,
+        function(data) {
+            preencherLista(data);
+        }
+    );
+})
+
+function temporizador() {
+    limparLista();
+    atualizaDados();
+}
+
+function atualizaDados() {
+    $.get(
+        getUrl,
+        function(data) {
+            preencherLista(data);
+        }
+    );
+}
+
+function limparLista() {
+    $("#tab").empty();
+}
+
+function preencherLista(data) {
+    var lista = $("#tab")[0];
+    data.map(function(dado) {
+        var item = criarItem(dado);
+        lista.innerHTML = item + lista.innerHTML;
+    });
+}
+
+function criarItem(data) {
+    var string = "";
+    var classe = "";
+
+    if (data.status == "off") {
+        classe = "list-group-item-danger";
+    } else {
+        classe = "list-group-item-success";
+    }
+
+    string = template(data.number, data.status, classe);
+
+    return string;
+}
+
+function template(number, status, classe) {
+    return `
+    <tr id="linha">
+        <th name="number" scope="row">${number}</th>
+        <td name="status" class="${classe}">${status}</td>
+    </tr>
+    `;
+}
